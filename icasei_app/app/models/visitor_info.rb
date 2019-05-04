d86a4dffea48bcc938d54e76e4d554316686723d
Felipe Ramos <pipoblakgamer@gmail.com>
2019-05-04 18:34:39 -0300

    Visitor Info Validations and Tests

--- icasei_app/app/models/visitor_info.rb
+++ icasei_app/app/models/visitor_info.rb
@@ -1,4 +1,7 @@
 class VisitorInfo < ApplicationRecord
+  validates :guid, presence: true 
+  validates :path, presence: true
+  validates :accessed_at, presence: true
   ACCESSIBLE_ATTRIBUTES = [:guid, :path, :accessed_at].freeze
 
 end
--- icasei_app/spec/models/visitor_info_spec.rb
+++ icasei_app/spec/models/visitor_info_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe VisitorInfo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "VisitorInfo is valid when guid is present, path is present, accessed_at is present" do
+    visitor_info = VisitorInfo.new(guid: "824d438a-ea7d-03dc-60dc-829d4eee2527", path:"/rspec", accessed_at: Time.now)
+    expect(visitor_info).to be_valid 
+  end
+
+  it "VisitorInfo is Invalid when guid is missing" do
+    visitor_info = VisitorInfo.new(path:"/rspec", accessed_at: Time.now)
+    expect(visitor_info).to be_invalid 
+  end
+
+  it "VisitorInfo is Invalid when path is missing" do
+    visitor_info = VisitorInfo.new(guid: "824d438a-ea7d-03dc-60dc-829d4eee2527", accessed_at: Time.now)
+    expect(visitor_info).to be_invalid 
+  end
+
+  it "VisitorInfo is Invalid when accessed_at is missing" do
+    visitor_info = VisitorInfo.new(guid: "824d438a-ea7d-03dc-60dc-829d4eee2527", path:"/rspec")
+    expect(visitor_info).to be_invalid 
+  end
+
 end
