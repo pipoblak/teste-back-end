d0a0097a7a2e10d18fea888cdf35b7f2071a4f9e
Felipe Ramos <pipoblakgamer@gmail.com>
2019-05-03 21:07:10 -0300

    Adding Bootstrap, and Base Controller Default Actions

--- icasei_app/app/controllers/base_controller.rb
+++ icasei_app/app/controllers/base_controller.rb
@@ -5,18 +5,28 @@ class BaseController < ApplicationController
   def index
   end
 
-  def show
-  end 
-
-  def new
-  end 
-
   def create
+    create! do |_success, failure|
+      failure.html { redirect_to new_resource_path, alert: resource.errors.full_messages.join(', ') } if resource.errors.any?
+    end
   end
 
   def update
-  end 
+    update! do |_success, failure|
+      failure.html { redirect_to edit_resource_path, alert: resource.errors[:base].join(', ') } if resource.errors[:base].any?
+    end
+  end
 
   def destroy
+    destroy! do |_success, failure|
+      failure.html { redirect_to collection_path, alert: resource.errors[:base].join(', ') } if resource.errors[:base].any?
+    end
   end
+
+  private
+
+  def resource_params
+    params.require(resource_class.model_name.to_s.underscore.to_sym).permit(resource_class::ACCESSIBLE_ATTRIBUTES)
+  end
+
 end
