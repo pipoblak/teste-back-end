9ee611a03d588f58c5b63619627a137bd24343c4
Felipe Ramos <pipoblakgamer@gmail.com>
2019-05-05 23:30:36 -0300

    User Contact tests and validations

--- icasei_app/app/models/user_contact.rb
+++ icasei_app/app/models/user_contact.rb
@@ -1,2 +1,5 @@
 class UserContact < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
+
 end
--- icasei_app/spec/models/user_contact_spec.rb
+++ icasei_app/spec/models/user_contact_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe UserContact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "UserContact is valid when email is unique and present, name is present" do
+    user_contact = UserContact.new(email: "test@test.com.br", name: "Contact Test")
+    expect(user_contact).to be_valid 
+  end
+
+  it "UserContact is invalid when email is not unique" do
+    UserContact.create(email: "test@test.com.br", name: "Contact Test 1")
+    user_contact = UserContact.new(email: "test@test.com.br", name: "Contact Test 2")
+    expect(user_contact).to be_invalid 
+  end
+
+  it "UserContact is invalid when email is not present" do
+    user_contact = UserContact.new(name: "Contact Test")
+    expect(user_contact).to be_invalid 
+  end
+
+  it "UserContact is invalid when name is not present" do
+    user_contact = UserContact.new(email: "test@test.com.br")
+    expect(user_contact).to be_invalid 
+  end
 end
