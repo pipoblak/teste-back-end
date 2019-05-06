9b2bd95e4eb0784471459cea62ea8a1911e1c53a
Felipe Ramos <pipoblakgamer@gmail.com>
2019-05-06 00:18:56 -0300

    Changing Order Attribute and Showing Created At on Visitor Infos

--- icasei_app/app/controllers/visitor_infos_controller.rb
+++ icasei_app/app/controllers/visitor_infos_controller.rb
@@ -1,7 +1,7 @@
 class VisitorInfosController < BaseController
   skip_before_action :verify_authenticity_token
   def index
-    @visitor_infos = VisitorInfo.all.order(accessed_at: :desc).page(params[:page]).per(50)
+    @visitor_infos = VisitorInfo.all.order(created_at: :desc).page(params[:page]).per(50)
   end 
   def create
     Thread.new {
--- icasei_app/app/models/visitor_info.rb
+++ icasei_app/app/models/visitor_info.rb
@@ -4,6 +4,6 @@ class VisitorInfo < ApplicationRecord
   validates :accessed_at, presence: true
   
   ACCESSIBLE_ATTRIBUTES = [:guid, :path, :accessed_at].freeze
-  ATTRIBUTES_FOR_PRESENTATION_TABLE = [:id, :guid, :path, :accessed_at].freeze
+  ATTRIBUTES_FOR_PRESENTATION_TABLE = [:id, :guid, :path, :accessed_at, :created_at].freeze
   SUBTITLE = "Informations from Visitors"
 end
